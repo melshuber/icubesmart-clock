3e3b294081f778b108be3e2b161ddc444105f1c3
melshuber <20516615+melshuber@users.noreply.github.com>
2021-04-07 14:18:44 +0200

    main: rewrite main

--- main.c
+++ main.c
@@ -3,17 +3,38 @@
 #include "uart.h"
 #include "board.h"
 #include "cpu.h"
+#include "render.h"
 #include "sim.h"
+#include "fixed-point.h"
+#include <math.h>
 
-#define T2CON_TR2 (1 << 2)
+void test();
 
-void render_frame(__xdata fb_frame_t *frame)
+void render_frame(__xdata fb_frame_t *fb)
 {
-	uint8_t i;
+	static __xdata fp_t a = FP_sPI/16;//xbFP_0;
+	static __xdata mat4x4_t m;
 
-	for (i = 0; i < sizeof(frame); i++) {
-		frame->pixels[i] = i;
+	fp_identity_mat4x4(&m);
+	fp_pre_rotate_mat4x4_z(&m, a);
+
+	render_clear(fb);
+	render_tex2D(fb, &tex2, &m);
+
+	a = fp_add(a, FP_sPI/16);
+}
+
+void bench_render_tex2d(__xdata fb_frame_t *fb)
+{
+	int8_t i;
+	static __xdata mat4x4_t m;
+	fp_identity_mat4x4(&m);
+
+	for (i = 0; i < 100; i++) {
+		render_tex2D(fb, &tex2, &m);
+		sim_put_uint8(i);
 	}
+	sim_stop();
 }
 
 void main(void)
@@ -21,16 +42,22 @@ void main(void)
 	while (!sim_detect());
 	sim_puts("SIMULATION\n");
 
+	//bench_render_tex2d(fb_back_frame());
+
 	EA = 1;
 	uart_init();
 	fb_init();
 
 	uart_puts("UART active\n");
+	int i=0;
 
 	while (1) {
 		if (!fb_back_frame_complete()) {
 			render_frame(fb_back_frame());
-			fb_back_frame_completed();			
+			render_sim_printfb(fb_back_frame());
+			fb_back_frame_completed();
+			i++;
+			sim_put_uint8(i);
 		}
 	}
 }

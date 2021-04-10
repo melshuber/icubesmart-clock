3df9206020c59f52ab41063c740d47ab9b026d15
melshuber <20516615+melshuber@users.noreply.github.com>
2021-04-10 11:26:29 +0200

    cpu.c: No longer default defined registers
    This is (a) not necessary since they are set to default on reset,
    and (b) some have been wrong anyways (e.g.: P1 should be 0).

--- src/cpu.c
+++ src/cpu.c
@@ -1,10 +1,10 @@
 /* GPIOs to 3d Matrix Display System */
-__sfr __at(0x80) P0 = 0;
-__sfr __at(0x90) P1 = 0;
-__sfr __at(0xA0) P2 = 0;
+__sfr __at(0x80) P0;
+__sfr __at(0x90) P1;
+__sfr __at(0xA0) P2;
 
 /* Timer 0/1 Registers */
-__sfr __at(0x88) TCON = 0;
+__sfr __at(0x88) TCON;
 __sbit __at(0x88) IT0;
 __sbit __at(0x89) IE0;
 __sbit __at(0x8A) IT1;
@@ -13,22 +13,22 @@ __sbit __at(0x8C) TR0;
 __sbit __at(0x8D) TF0;
 __sbit __at(0x8E) TR1;
 __sbit __at(0x8F) TF1;
-__sfr __at(0x89) TMOD = 0;
-__sfr __at(0x8A) TL0 = 0;
-__sfr __at(0x8B) TL1 = 0;
-__sfr __at(0x8C) TH0 = 0;
-__sfr __at(0x8D) TH1 = 0;
+__sfr __at(0x89) TMOD;
+__sfr __at(0x8A) TL0;
+__sfr __at(0x8B) TL1;
+__sfr __at(0x8C) TH0;
+__sfr __at(0x8D) TH1;
 
 /* Timer 2 Registers */
-__sfr __at(0xC8) T2CON = 0;
-__sfr __at(0xC9) T2MOD = 0;
-__sfr __at(0xCA) RCAP2L = 0;
-__sfr __at(0xCB) RCAP2H = 0;
-__sfr __at(0xCC) TL2 = 0;
-__sfr __at(0xCD) TH2 = 0;
+__sfr __at(0xC8) T2CON;
+__sfr __at(0xC9) T2MOD;
+__sfr __at(0xCA) RCAP2L;
+__sfr __at(0xCB) RCAP2H;
+__sfr __at(0xCC) TL2;
+__sfr __at(0xCD) TH2;
 
 /* UART Registers */
-__sfr __at(0x98) SCON = 0;
+__sfr __at(0x98) SCON;
 __sbit __at(0x98) RI;
 __sbit __at(0x99) TI;
 __sbit __at(0x9A) RB8;

af537bc6be2928a6fffd7f0e90d849956eaea49f
melshuber <20516615+melshuber@users.noreply.github.com>
2021-04-11 12:38:53 +0200

    main: Add support for multiple modes
    Prepare to add for multiple entry modes. E.g.: Clock Display, Settime,
    ...

--- src/main.c
+++ src/main.c
@@ -28,18 +28,28 @@ static __xdata union {
 		mat4x4_t m;
 		vec3_t v;
 		tex2D_t texture;
-	} render_digits;
+	} mode_clock_render;
 } _xso;
 
-typedef struct {
-	uint16_t old_digits;
-	uint16_t new_digits;
-	uint16_t ticks;
-	uint8_t step;
-	uint8_t pending;
-} digits_animation_t;
+typedef enum {
+	MODE_CLOCK,
+} mode_t;
 
-static __xdata digits_animation_t _digits_animation;
+typedef struct {
+	mode_t mode;
+	time_t time;
+	union {
+		struct {
+			uint16_t start_tick;
+			uint8_t step;
+			uint16_t old_digits;
+			uint16_t new_digits;
+			uint8_t pending;
+		} mode_clock;
+	};
+} display_state_t;
+
+static __xdata display_state_t _display_state;
 
 #define XSO(X) _xso.OVERLAY_FIELD.X
 #define xso_m XSO(m)
@@ -106,40 +116,40 @@ void bench_render_tex2d(__xdata fb_frame_t *fb)
 }
 #undef OVERLAY_FIELD
 
-#define OVERLAY_FIELD render_digits
-void render_digits(__xdata fb_frame_t *fb) __reentrant
+static const mat4x4_t mat_rotate = {
+	{  FP_0,  FP_1,	 FP_0,	FP_0, },
+	{  FP_0,  FP_0, -FP_1,	FP_0, },
+	{ -FP_1,  FP_0,	 FP_0,	FP_0, },
+	{  FP_0,  FP_0,	 FP_0,	FP_1, },
+};
+
+static const vec4_t pos[4] = {
+	{ fp_add(FP_3, FP_1/2), FP_1, FP_0 },
+	{ fp_add(FP_1, FP_1/2), -FP_1, FP_0 },
+	{ fp_add(-FP_1, -FP_1/2), FP_1, FP_0 },
+	{ fp_add(-FP_3, -FP_1/2), -FP_1, FP_0 },
+};
+
+#define OVERLAY_FIELD mode_clock_render
+static void _mode_clock_render(__xdata fb_frame_t *fb) __reentrant
 {
 	int i;
 
-	static const mat4x4_t mat_rotate = {
-		{  FP_0,  FP_1,	 FP_0,	FP_0, },
-		{  FP_0,  FP_0, -FP_1,	FP_0, },
-		{ -FP_1,  FP_0,	 FP_0,	FP_0, },
-		{  FP_0,  FP_0,	 FP_0,	FP_1, },
-	};
-
-	static const vec4_t pos[4] = {
-		{ fp_add(FP_3, FP_1/2), FP_1, FP_0 },
-		{ fp_add(FP_1, FP_1/2), -FP_1, FP_0 },
-		{ fp_add(-FP_1, -FP_1/2), FP_1, FP_0 },
-		{ fp_add(-FP_3, -FP_1/2), -FP_1, FP_0 },
-	};
-
-	uint16_t new_digits = _digits_animation.new_digits;
-	uint16_t old_digits = _digits_animation.old_digits;
+	uint16_t new_digits = _display_state.mode_clock.new_digits;
+	uint16_t old_digits = _display_state.mode_clock.old_digits;
 
 	for (i = 0; i < 4; i++) {
 		uint8_t new_digit = new_digits & 0xf;
 		uint8_t old_digit = old_digits & 0xf;
 
 		memcpy(&XSO(v), &pos[i], sizeof(vec3_t));
-		if ((new_digit == old_digit) || (_digits_animation.step > 8)) {
+		if ((new_digit == old_digit) || (_display_state.mode_clock.step > 8)) {
 			memcpy(&XSO(texture),
 			       font_get_texture('0' + new_digit),
 			       sizeof(tex2D_t));
 		} else {
 			if (XSO(v)[0] > 0) {
-				XSO(v)[0] = fp_add(XSO(v)[0], FP_FROM_INT(_digits_animation.step));
+				XSO(v)[0] = fp_add(XSO(v)[0], FP_FROM_INT(_display_state.mode_clock.step));
 				if (XSO(v)[0] > FP_4) {
 					XSO(v)[0] = fp_sub(XSO(v)[0], FP_FROM_INT(8));
 					memcpy(&XSO(texture),
@@ -151,7 +161,7 @@ void render_digits(__xdata fb_frame_t *fb) __reentrant
 					       sizeof(tex2D_t));
 				}
 			} else {
-				XSO(v)[0] = fp_sub(XSO(v)[0], FP_FROM_INT(_digits_animation.step));
+				XSO(v)[0] = fp_sub(XSO(v)[0], FP_FROM_INT(_display_state.mode_clock.step));
 				if (XSO(v)[0] < -FP_4) {
 					XSO(v)[0] = fp_add(XSO(v)[0], FP_FROM_INT(8));
 					memcpy(&XSO(texture),
@@ -174,9 +184,73 @@ void render_digits(__xdata fb_frame_t *fb) __reentrant
 		new_digits >>= 4;
 		old_digits >>= 4;
 	}
-	_digits_animation.pending = 0;
+	_display_state.mode_clock.pending = 0;
 }
 #undef OVERLAY_FIELD
+
+static uint16_t _mode_clock_digits()
+{
+	uint16_t tmp;
+	tmp = ((uint16_t)_display_state.time.hour_bcd) << 8;
+	tmp |= _display_state.time.min_bcd;
+	return tmp;
+}
+
+static void _mode_clock_setup()
+{
+	uint16_t tmp = _mode_clock_digits();
+	_display_state.mode = MODE_CLOCK;
+	_display_state.mode_clock.old_digits = tmp;
+	_display_state.mode_clock.new_digits = tmp;
+	_display_state.mode_clock.step = 9;
+	_display_state.mode_clock.start_tick = 0;
+	_display_state.mode_clock.pending = 1;
+}
+
+static void _mode_clock_handle()
+{
+	uint16_t tmp;
+
+	/* Check if a back framebuffer is available */
+	if (fb_back_frame_complete()) {
+		/* early exit if not */
+		return;
+	}
+
+	/* update animation */
+	if (_display_state.mode_clock.step > 8) {
+		tmp = _mode_clock_digits();
+
+		if (_display_state.mode_clock.new_digits != tmp) {
+			_display_state.mode_clock.old_digits =
+				_display_state.mode_clock.new_digits;
+			_display_state.mode_clock.new_digits = tmp;
+			_display_state.mode_clock.start_tick = _display_state.time.ticks;
+			_display_state.mode_clock.step = 0;
+			_display_state.mode_clock.pending = 1;
+		}
+	} else {
+		tmp = _display_state.time.ticks - _display_state.mode_clock.start_tick;
+		uint8_t tmp2 = _display_state.mode_clock.step;
+		if (tmp >= 36) {
+			_display_state.mode_clock.step = 9;
+		} else {
+			_display_state.mode_clock.step = ((uint8_t)tmp) / 4;
+		}
+		if (tmp2 != _display_state.mode_clock.step) {
+			_display_state.mode_clock.pending = 1;
+		}
+	}
+
+	/* render frame */
+	if (_display_state.mode_clock.pending != 0) {
+		__xdata fb_frame_t *fb = fb_back_frame();
+		render_clear(fb);
+		_mode_clock_render(fb);
+		fb_back_frame_completed();
+		note("f\n");
+	}
+}
 
 void main(void)
 {
@@ -197,61 +271,19 @@ void main(void)
 	pca_init();
 	printf("PCA initialized\n");
 
-	uint16_t tmp;
-	time_t time;
-	time_get(&time);
+	time_get(&_display_state.time);
 
 	uint8_t i=0;
-	_digits_animation.old_digits = 0xffff;
-	_digits_animation.new_digits = 0xffff;
-	_digits_animation.step = 9;
-	_digits_animation.ticks = time.ticks;
-	_digits_animation.pending = 1;
+	_mode_clock_setup();
 
 	for (;;PCON |= 1) {
-		if (fb_back_frame_complete()) {
-			continue;
-		}
-
-		time_get(&time);
-
-		__xdata fb_frame_t *fb = fb_back_frame();
-
-		if (_digits_animation.step > 8) {
-			tmp = ((uint16_t)time.hour_bcd) << 8;
-			tmp |= time.min_bcd;
-
-			if (_digits_animation.new_digits != tmp) {
-				_digits_animation.old_digits = _digits_animation.new_digits;
-				_digits_animation.new_digits = tmp;
-				_digits_animation.ticks = time.ticks;
-				_digits_animation.step = 0;
-				_digits_animation.pending = 1;
-			}
-		} else {
-			tmp = time.ticks - _digits_animation.ticks;
-			uint8_t tmp2 = _digits_animation.step;
-			if (tmp >= 36) {
-				_digits_animation.step = 9;
-			} else {
-				_digits_animation.step = ((uint8_t)tmp) / 4;
-			}
-			if (tmp2 != _digits_animation.step) {
-				_digits_animation.pending = 1;
-			}
-		}
-
-		if (_digits_animation.pending != 0) {
-			render_clear(fb);
-
-			uint16_t ticks = time.ticks;
-			render_digits(fb);
-
-			time_get(&time);
-
-			fb_back_frame_completed();
-			note("f\n");
-			i++;
+		time_get(&_display_state.time);
+		switch (_display_state.mode) {
+		default:
+			_mode_clock_setup();
+		case MODE_CLOCK:
+			_mode_clock_handle();
+			break;
 		}
 	}
 }

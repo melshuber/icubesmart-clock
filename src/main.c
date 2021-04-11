88725494c27a58c90a5df44b7cdef0aed3ee098f
melshuber <20516615+melshuber@users.noreply.github.com>
2021-04-11 14:42:39 +0200

    main: Add mode to set time using the three keys

--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@
 #include "font.h"
 #include "key.h"
 
+#define CLOCK_DISPLAY_FIELD_LO min_bcd
+#define CLOCK_DISPLAY_FIELD_HI hour_bcd
+
 static __xdata union {
 	struct {
 		mat4x4_t m;
@@ -29,10 +32,16 @@ static __xdata union {
 		vec3_t v;
 		tex2D_t texture;
 	} mode_clock_render;
+	struct {
+		mat4x4_t m;
+		vec3_t v;
+		tex2D_t texture;
+	} mode_settime_render;
 } _xso;
 
 typedef enum {
 	MODE_CLOCK,
+	MODE_SETTIME,
 } mode_t;
 
 typedef struct {
@@ -47,10 +56,16 @@ typedef struct {
 			uint8_t pending;
 			uint8_t dark;
 		} mode_clock;
+		struct {
+			uint16_t digits;
+			uint8_t current_digit;
+		} mode_settime;
 	};
 } display_state_t;
 
 static __xdata display_state_t _display_state;
+
+static void _next_mode();
 
 #define XSO(X) _xso.OVERLAY_FIELD.X
 #define xso_m XSO(m)
@@ -192,8 +207,8 @@ static void _mode_clock_render(__xdata fb_frame_t *fb) __reentrant
 static uint16_t _mode_clock_digits()
 {
 	uint16_t tmp;
-	tmp = ((uint16_t)_display_state.time.hour_bcd) << 8;
-	tmp |= _display_state.time.min_bcd;
+	tmp = ((uint16_t)_display_state.time.CLOCK_DISPLAY_FIELD_HI) << 8;
+	tmp |= _display_state.time.CLOCK_DISPLAY_FIELD_LO;
 	return tmp;
 }
 
@@ -214,6 +229,9 @@ static void _mode_clock_handle()
 	uint16_t tmp;
 
 	switch (key_consume_event()) {
+	case KEY_MAKE_EVENT(KEY_NEXT, KEY_LONG):
+		_next_mode();
+		return;
 	case KEY_MAKE_EVENT(KEY_START, KEY_PRESS):
 		_display_state.mode_clock.dark ^= 1;
 		_display_state.mode_clock.pending = 1;
@@ -264,6 +282,146 @@ static void _mode_clock_handle()
 	}
 }
 
+#define OVERLAY_FIELD mode_settime_render
+static void _mode_settime_render(__xdata fb_frame_t *fb) __reentrant
+{
+	int i;
+
+	uint16_t digits = _display_state.mode_settime.digits;
+
+	for (i = 0; i < 4; i++, digits >>= 4) {
+		if ((i == _display_state.mode_settime.current_digit) &&
+		    (_display_state.time.tick < TIME_TICK_HZ / 2)) {
+			continue;
+		}
+
+		uint8_t digit = digits & 0xf;
+
+		memcpy(&XSO(texture),
+		       font_get_texture('0' + digit),
+		       sizeof(tex2D_t));
+
+		memcpy(&XSO(m), &mat_rotate, sizeof(mat4x4_t));
+		fp_post_translate_mat4x4_vec3(&XSO(m), &pos[i]);
+
+		render_tex2D(fb, &XSO(texture), &XSO(m));
+
+
+	}
+}
+#undef OVERLAY_FIELD
+
+static void _mode_settime_setup()
+{
+	_display_state.mode = MODE_SETTIME;
+	_display_state.mode_settime.digits = _mode_clock_digits();
+	_display_state.mode_settime.current_digit = 0;
+}
+
+static void _mode_settime_change_digit(bool increment) __critical
+{
+	uint8_t tmp;
+	time_get(&_display_state.time);
+
+	switch (_display_state.mode_settime.current_digit) {
+	case 0:
+		tmp = _display_state.time.CLOCK_DISPLAY_FIELD_LO;
+		_display_state.time.CLOCK_DISPLAY_FIELD_LO &= 0xf0;
+		break;
+	case 1:
+		tmp = _display_state.time.CLOCK_DISPLAY_FIELD_LO;
+		tmp >>= 4;
+		_display_state.time.CLOCK_DISPLAY_FIELD_LO &= 0xf;
+		break;
+	case 2:
+		tmp = _display_state.time.CLOCK_DISPLAY_FIELD_HI;
+		_display_state.time.CLOCK_DISPLAY_FIELD_HI &= 0xf0;
+		break;
+	case 3:
+		tmp = _display_state.time.CLOCK_DISPLAY_FIELD_HI;
+		tmp >>= 4;
+		_display_state.time.CLOCK_DISPLAY_FIELD_HI &= 0xf;
+		break;
+	default:
+		return;
+	}
+
+	if (increment) {
+		if (tmp == 9) {
+			tmp = 0;
+		} else {
+			tmp++;
+		}
+	} else {
+		if (tmp == 0) {
+			tmp = 9;
+		} else {
+			tmp--;
+		}
+	}
+
+	if (_display_state.mode_settime.current_digit & 1) {
+		tmp <<= 4;
+	}
+
+	if ((_display_state.mode_settime.current_digit & 2) == 0) {
+		_display_state.time.CLOCK_DISPLAY_FIELD_LO |= tmp;
+	} else {
+		_display_state.time.CLOCK_DISPLAY_FIELD_HI |= tmp;
+	}
+
+	time_set(&_display_state.time);
+}
+
+static void _mode_settime_handle()
+{
+	switch (key_consume_event()) {
+	case KEY_MAKE_EVENT(KEY_NEXT, KEY_LONG):
+		_next_mode();
+		return;
+	case KEY_MAKE_EVENT(KEY_START, KEY_PRESS):
+		_mode_settime_change_digit(false);
+		break;
+	case KEY_MAKE_EVENT(KEY_CYCLE, KEY_PRESS):
+		_mode_settime_change_digit(true);
+		break;
+	case KEY_MAKE_EVENT(KEY_NEXT, KEY_PRESS):
+		_display_state.mode_settime.current_digit++;
+		_display_state.mode_settime.current_digit %= 4;
+		break;
+	default:
+		break;
+	}
+
+	_display_state.mode_settime.digits = _mode_clock_digits();
+
+	/* Check if a back framebuffer is available */
+	if (fb_back_frame_complete()) {
+		/* early exit if not */
+		return;
+	}
+
+	/* render frame */
+	__xdata fb_frame_t *fb = fb_back_frame();
+	render_clear(fb);
+	_mode_settime_render(fb);
+	fb_back_frame_completed();
+}
+
+
+static void _next_mode()
+{
+	switch (_display_state.mode) {
+	default:
+	case MODE_CLOCK:
+		_mode_settime_setup();
+		break;
+	case MODE_SETTIME:
+		_mode_clock_setup();
+		break;
+	}
+}
+
 void main(void)
 {
 	EA = 1;
@@ -290,11 +448,15 @@ void main(void)
 
 	for (;;PCON |= 1) {
 		time_get(&_display_state.time);
+
 		switch (_display_state.mode) {
 		default:
 			_mode_clock_setup();
 		case MODE_CLOCK:
 			_mode_clock_handle();
+			break;
+		case MODE_SETTIME:
+			_mode_settime_handle();
 			break;
 		}
 	}
